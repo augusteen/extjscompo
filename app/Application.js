d5e072ab52b41c7b5c4dd3439606eab8d427d51f
augusteen <austin.chellian@gmail.com>
2016-11-18 16:07:21 +0530

    Preference related work

--- app/Application.js
+++ app/Application.js
@@ -7,33 +7,34 @@ Ext.define('testApp.Application', {
     extend: 'Ext.app.Application',
     name: 'testApp',
     controllers: [
-            'Root'
-        ],
+        'Root'
+    ],
     /**
      * @requires testApp.controller.Root
      */
     requires: [
-        'testApp.controller.Root','testApp.view.login.LoginController'
+        'testApp.controller.Root', 'testApp.view.login.LoginController'
     ],
     stores: [
-     
+
     ],
 
-    views :[
+    views: [
         'testApp.view.login.Login',
         'testApp.view.main.Main'
     ],
-    
-    launch: function () {
 
-       var token = localStorage.getItem('user-token');
+    launch: function() {
+
+        Ext.state.Manager.setProvider(new Ext.state.LocalStorageProvider());
+        var token = localStorage.getItem('user-token');
 
         // loggedIn = localStorage.getItem("TutorialLoggedIn");
 
         Ext.create({
             xtype: token ? 'app-main' : 'login'
-            // xtype : 'app-main'
-            // renderTo: Ext.getBody()
+                // xtype : 'app-main'
+                // renderTo: Ext.getBody()
         });
     }
 });
--- app/view/preference/Preference.js
+++ app/view/preference/Preference.js
@@ -12,53 +12,109 @@ Ext.define('testApp.view.preference.Preference', {
     "tooltip": "Preferences",
     "menu": {
         "xtype": "menu",
-        "stateEvents": ["beforeclose"],
-        "stateful": true,
-        "stateId": 'menuPreference',
+        preference: [{
+            "Name": "Shelf",
+            "UOM": "$",
+            "Code": "Shelf"
+        }, {
+            "Name": "RM %",
+            "UOM": "%",
+            "Code": "RM_PERC"
+        }, {
+            "Name": "NBC",
+            "UOM": "$",
+            "Code": "Net_List_per_Selling_Unit_ATAX"
+        }, {
+            "Name": "Net List",
+            "UOM": "$",
+            "Code": "Net_List_ATAX"
+        }, {
+            "Name": "NBC Before Tax",
+            "UOM": "$",
+            "Code": "Net_List_per_Selling_Unit"
+        }, {
+            "Name": "Net List Before Tax",
+            "UOM": "$",
+            "Code": "Net_List"
+        }, {
+            "Name": "DM %",
+            "UOM": "%",
+            "Code": "DM_PERC"
+        }, {
+            "Name": "Landed",
+            "UOM": "$",
+            "Code": "Landed"
+        }, {
+            "Name": "State Tax",
+            "UOM": "$",
+            "Code": "State Tax"
+        }, {
+            "Name": "Freight",
+            "UOM": "$",
+            "Code": "Freight"
+        }, {
+            "Name": "Net FOB",
+            "UOM": "$",
+            "Code": "Net_FOB"
+        }, {
+            "Name": "SDA",
+            "UOM": "$",
+            "Code": "SDA"
+        }, {
+            "Name": "FOB",
+            "UOM": "$",
+            "Code": "Bill_FOB"
+        }, {
+            "Name": "RAB",
+            "UOM": "$",
+            "Code": "RAB"
+        }, {
+            "Name": "SKU Count",
+            "UOM": "",
+            "Code": "SKU_Count"
+        }, {
+            "Name": "Dist Count",
+            "UOM": "",
+            "Code": "Distributor_Count"
+        }],
+        getConfigForPreferenceWindow: function(pFactsList) {
+            var items = [];
+            var state = Ext.state.Manager.get('menuPreferencestate')
+            for (var l_facts in pFactsList) {
+                var l_object = {};
+                l_object["fieldLabel"] = "";
+                l_object["boxLabel"] = pFactsList[l_facts]["Name"];
+                l_object["name"] = pFactsList[l_facts]["Code"];
+                l_object["cls"] = "clsPPCheckBoxes";
+                if(state.value[pFactsList[l_facts]["Code"]])
+                l_object["checked"] = true;
+                items.push(JSON.parse(JSON.stringify(l_object)));
+            }
+
+            return items;
+        },
+
         "cls": "clsMenuPreference",
         "id": "menuPreference",
         "width": 390,
         "defaultAlign": "tr-bl",
-        listeners:{
-            afterShow: function(){
-                console.log('shown');
-                 
-            },
-            afterRender: function(){
-                console.log('expand');
-                var obj_PricePlanPrefrenceForm = Ext.getCmp("formPricePlanPrefrence").getForm();
-            obj_PricePlanPrefrenceForm.setValues({
-                "All": false,
-                "Shelf": true,
-                "Selling Units": true,
-                "RM %": true,
-                "RMU %": true,
-                "Net List ATAX per SU": true,
-                "Net List ATAX": true,
-                "Net List per SU": true,
-                "Net List": true,
-                "DM %": true,
-                "DMU %": true,
-                "Landed": true,
-                "Net FOB": true,
-                "DA": true,
-                "SDA": true,
-                "WP": true,
-                "FOB": true,
-                "RAB": false,
-                "RAB (Less Bkg)": false,
-                "Scan": true,
-                "Landed": true,
-                "Net List Before Tax": true,
-                "Net Btl Cost Before Tax": true,
-                "Net Btl Cost": true,
-                "sort": "Descending"
+        listeners: {
+            // beforeshow: function() {
+            //     console.log('shown');
 
-            });
+            // },
+            afterRender: function() {
+                console.log('expand');
+                // var obj_PricePlanPrefrenceForm = Ext.getCmp("formPricePlanPrefrence").getForm();
+
+                var l_configCheckGroup = Ext.getCmp("chkGrpPricePlanPrefrence");
+                l_configCheckGroup.removeAll();
+                var l_prefrenceWindowconfig = this.getConfigForPreferenceWindow(this.preference);
+                l_configCheckGroup.add(l_prefrenceWindowconfig);
             }
         },
-        afterShow: function(){
-            console.log('show');
+        afterShow: function() {
+            // console.log('show');
         },
         "items": [{
             "xtype": "panel",
@@ -129,6 +185,39 @@ Ext.define('testApp.view.preference.Preference', {
                                     }, {
                                         "xtype": "checkboxgroup",
                                         "id": "chkGrpPricePlanPrefrence",
+                                        "stateful": true,
+                                        "stateId": 'menuPreferencestate',
+                                        stateEvents: ['change'],
+                                        listeners: {
+                                            // change: function() {
+                                            //     console.log('shown');
+
+                                            // },
+                                            afterrender: function(){
+                                                console.log('afterender');
+                                                // var state = Ext.state.Manager.get('menuPreferencestate');
+                                                // // var l_configCheckGroup = Ext.getCmp("chkGrpPricePlanPrefrence");
+                                                // if(state.value)
+                                                // this.setValue(state.value);
+
+                                            }
+                                        },
+                                        getState: function() {
+                                            var state = Ext.form.CheckboxGroup.prototype.getState()
+                                            // var l_configCheckGroup = Ext.getCmp("chkGrpPricePlanPrefrence");
+                                            state = this.addPropertyToState(state,'value',this.getValue())
+                                            return state;
+                                        },
+                                        applyState: function(state) {
+
+                                            // Ext.apply()
+                                            if(state){
+                                            // var l_configCheckGroup = Ext.getCmp("chkGrpPricePlanPrefrence");
+                                            this.setValue(state.value);
+                                            console.log('Menu State' + state);
+                                            Ext.apply(this,state.value);
+                                            }
+                                        },
                                         "columns": [115,
                                             115,
                                             115
